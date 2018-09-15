bfe39320419230f1c89d3c3a651cc2b95d6b061b
Anton <anton@adc.sh>
2018-09-15 03:02:17 +0300

    build

--- build/index.js
+++ build/index.js
@@ -1,26 +1,66 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('@wrote/clone')
+let ensurePath = require('@wrote/ensure-path'); if (ensurePath && ensurePath.__esModule) ensurePath = ensurePath.default;
+let readDirStructure = require('@wrote/read-dir-structure'); if (readDirStructure && readDirStructure.__esModule) readDirStructure = readDirStructure.default;
+let makePromise = require('makepromise'); if (makePromise && makePromise.__esModule) makePromise = makePromise.default;
+const { createReadStream, createWriteStream, lstat } = require('fs');
+const { join, basename } = require('path');
 
 /**
- * A package to clone a file or directory.
- * @param {Config} config Options for the program.
- * @param {boolean} config.shouldRun A boolean option.
+ * Clone a file.
+ * @param {string} from Path to the file which to clone.
+ * @param {string} to Path to the file where to clone.
  */
-               async function clone(config) {
-  const {
-    type,
-  } = config
-  LOG('@wrote/clone called with %s', type)
-  return type
+const cloneFile = async (from, to) => {
+  const rs = createReadStream(from)
+  const ws = createWriteStream(to)
+  rs.pipe(ws)
+  await Promise.all([
+    new Promise((r, j) => {
+      rs.on('close', r).on('error', j)
+    }),
+    new Promise((r, j) => {
+      ws.on('close', r).on('error', j)
+    }),
+  ])
 }
 
-/* documentary types/index.xml */
 /**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} shouldRun A boolean option.
+ * Clones a directory.
+ * @param {string} from Path of the directory being cloned.
+ * @param {string} to Path to the cloned directory (not its parent!).
  */
+const cloneDir = async (from, to) => {
+  await ensurePath(join(to, 'path.file'))
+  const { content } = await readDirStructure(from)
+  const pr = Object.keys(content).map(async (k) => {
+    const { type } = content[k]
+    const p = join(from, k)
+    const pt = join(to, k)
+    if (type == 'Directory') {
+      await cloneDir(p, pt)
+    } else if (type == 'File') {
+      await cloneFile(p, pt)
+    }
+  })
+  await Promise.all(pr)
+}
 
+/**
+ * Clone a file or directory.
+ * @param {string} path Path to the file or directory to clone.
+ * @param {string} to Path to the directory to contain the file or directory being cloned (not the path to the cloned entity).
+ */
+const clone = async (path, to) => {
+  const s = await makePromise(lstat, path)
+  const b = basename(path)
+  const t = join(to, b)
 
-module.exports = clone
+  if (s.isDirectory()) {
+    await cloneDir(path, t)
+  } else {
+    await ensurePath(t)
+    await cloneFile(path, t)
+  }
+}
+
+module.exports=clone
 //# sourceMappingURL=index.js.map
